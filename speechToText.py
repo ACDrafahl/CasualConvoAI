d9212dc07ad77ca95c4744a3d8ff187f95f4e5c7
Andy <acd7@iastate.edu>
2023-12-28 14:51:32 -0600

    0.0.2 Improved setup for recognition by overriding the overflow exception and changing the model

--- speechToText.py
+++ speechToText.py
@@ -1,16 +1,18 @@
 import json
 from vosk import Model, KaldiRecognizer
 import pyaudio
-model = Model(r"C:\Program Files (x86)\Vosk\vosk-model-en-us-0.22")
+# model = Model(r"C:\Program Files (x86)\Vosk\vosk-model-en-us-0.22")
+model = Model(r"C:\Program Files (x86)\Vosk\vosk-model-en-us-0.22-lgraph")
 recognizer = KaldiRecognizer(model, 16000)
-
 mic = pyaudio.PyAudio()
 stream = mic.open(format = pyaudio.paInt16, channels = 1, rate = 16000, input = True, frames_per_buffer = 8192)
 stream.start_stream()
 
-print("Running!")
+print("Mic is hot!")
 while True:
-    input = stream.read(4096)
+    input = stream.read(4096, exception_on_overflow=False)
+    if len(input) == 0:
+        break
     if recognizer.AcceptWaveform(input):
         text = json.loads(recognizer.Result())["text"]
         print(text)
