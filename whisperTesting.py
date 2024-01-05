a2942628e90aa81a00859928ae3f14c7c2f8db1f
Andy <acd7@iastate.edu>
2024-01-05 13:57:49 -0600

    Linked Whisper and GPT4All successfully

--- whisperTesting.py
+++ whisperTesting.py
@@ -1,8 +1,9 @@
 import keyboard
 from whisper_mic import WhisperMic
+from gpt4all import GPT4All
 space_pressed = False
 
-def on_spacebar_press(self):
+def on_spacebar_press(mic):
     global space_pressed
     global output
     if not space_pressed:
@@ -11,25 +12,31 @@ def on_spacebar_press(self):
         while keyboard.is_pressed('space'):
             output += mic.listen()
             print(output)
+        return output
 
-def on_spacebar_release(self):
+def on_spacebar_release(model):
     global space_pressed
     global output
     if space_pressed:
         print("release")
         space_pressed = False
-        print("Final: " + output)
+        prompt = output
+        print("Prompt: " + prompt)
+        response = model.generate(prompt=prompt, max_tokens=25, temp=0.7)
+        print(response)
         output = ""
 
 def main():
     running = True
-    global mic, output
+    model = GPT4All("mistral-7b-openorca.Q4_0.gguf")
     mic = WhisperMic("base", "cpu", True, False, 300, 2, False, True, "~/.cache/whisper", 1)
+    global output
     output = ""
-    keyboard.on_press_key('space', on_spacebar_press)
-    keyboard.on_release_key('space', on_spacebar_release)
+    with model.chat_session():
+        keyboard.on_press_key('space', lambda event: on_spacebar_press(mic))
+        keyboard.on_release_key('space', lambda event: on_spacebar_release(model))
 
-    keyboard.wait('esc')
+        keyboard.wait('esc')
 
 if __name__ == "__main__":
     main()
