d53ff805477f8b9bfd4e7f1f1dadc875194c01fe
Andy <acd7@iastate.edu>
2024-01-09 17:24:26 -0600

    Added a couple minor changes for testing purposes

--- whisperTesting.py
+++ whisperTesting.py
@@ -1,4 +1,5 @@
 import keyboard
+import os
 import subprocess
 import sounddevice as sd
 import soundfile as sf
@@ -7,16 +8,16 @@ from gpt4all import GPT4All
 
 space_pressed = False
 
-# Define the complete path to the piper.exe executable
+# define the complete path to the piper.exe executable
 piper_exe_path = 'C:\\Users\\acdga\\.cache\\piper\\piper.exe'
-# Define the complete path to the onnx model file
+# define the complete path to the onnx model file
 onnx_path = 'C:\\Users\\acdga\\.cache\\piper\\en_US-kusal-medium.onnx'
-# Define the complete path to the json config file
+# define the complete path to the json config file
 json_path = 'C:\\Users\\acdga\\.cache\\piper\\en_en_US_kusal_medium_en_US-kusal-medium.onnx.json'
-# Define the output file name
+# define the output file name
 speechOutput = 'speechOutput.wav'
-# Define the rest of the command without the text
-base_command = f'{piper_exe_path} -m {onnx_path} -c {json_path} -f {speechOutput}'
+# define the rest of the command without the text
+base_command = f'{piper_exe_path} -m {onnx_path} -c {json_path} -f {speechOutput}' # There may be a way to use this without loading in the voice every time
 
 def on_spacebar_press(mic):
     global space_pressed
@@ -37,28 +38,30 @@ def on_spacebar_release(model):
         space_pressed = False
         prompt = output
         print("Prompt: " + prompt)
-        response = model.generate(prompt=prompt, max_tokens=25, temp=0.7)
+        response = model.generate(prompt=prompt, max_tokens=100, temp=0.7)
         speech(response)
         print(response)
         output = ""
 
 def speech(text):
-    # Combine the base command and the dynamically changing text
+    # combine the base command and the dynamically changing text
     speak = f'echo "{text}" | {base_command}'
 
-    # Run the speech synthesis command in the console
+    # run the speech synthesis command in the console
     try:
         subprocess.run(speak, check=True, shell=True)
     except subprocess.CalledProcessError as e:
         print(f"Error: {e}")
     
-    # Extract data and sampling rate from file 
+    # extract data and sampling rate from file 
     data, fs = sf.read(speechOutput, dtype='float32')  
     sd.play(data, fs)
     # status = sd.wait()  # Wait until file is done playing
 
 def main():
-    running = True
+    # purging old output, might be causing problems. 
+    if os.path.exists("output.wav"):
+        os.remove("output.wav")
     model = GPT4All("mistral-7b-openorca.Q4_0.gguf")
     mic = WhisperMic("base", "cpu", True, False, 300, 2, False, True, "~/.cache/whisper", 1)
     global output
